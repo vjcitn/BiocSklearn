53406a7d2be3afc69954b389d63a4e145face519
Vince Carey <stvjc@channing.harvard.edu>
2020-02-05 05:55:42 -0500

    block tests on windows

--- tests/testthat/test_bcsk.R
+++ tests/testthat/test_bcsk.R
@@ -8,9 +8,11 @@ library(BiocSklearn)
 
 context("imports")
 test_that("SklearnEls imports complete", {
+if (.Platform$OS.type != "windows") {
  skstuff = SklearnEls()
  expect_true(is.list(skstuff))
  expect_true(all.equal(names(skstuff), c("np", "pd", "h5py", "skd", "skcl", "joblib")))
+ }
 })
       
 #
@@ -19,16 +21,18 @@ test_that("SklearnEls imports complete", {
 
 context("object retrieval")
 test_that("pyobj, skPCA, getTransformed work", {
- irloc = system.file("csv/iris.csv", package="BiocSklearn")
- irismat = SklearnEls()$np$genfromtxt(irloc, delimiter=',')
- skpi = skPCA(irismat)
- gt = getTransformed(skpi)
- expect_true(abs(gt[1,1]+2.684126)<.001)
- pp = pyobj(skpi)
- clpca = c("sklearn.decomposition.pca.PCA", "sklearn.decomposition.base._BasePCA", 
-"abc.NewBase", "sklearn.base.BaseEstimator", "sklearn.base.TransformerMixin", 
-"python.builtin.object")
- #expect_true(all.equal(class(pp), clpca)) # checking to see if this is all that fails on build system
+ if (.Platform$OS.type != "windows") {
+  irloc = system.file("csv/iris.csv", package="BiocSklearn")
+  irismat = SklearnEls()$np$genfromtxt(irloc, delimiter=',')
+  skpi = skPCA(irismat)
+  gt = getTransformed(skpi)
+  expect_true(abs(gt[1,1]+2.684126)<.001)
+  pp = pyobj(skpi)
+  clpca = c("sklearn.decomposition.pca.PCA", "sklearn.decomposition.base._BasePCA", 
+ "abc.NewBase", "sklearn.base.BaseEstimator", "sklearn.base.TransformerMixin", 
+ "python.builtin.object")
+  #expect_true(all.equal(class(pp), clpca)) # checking to see if this is all that fails on build system
+ }
  TRUE
 })
 
