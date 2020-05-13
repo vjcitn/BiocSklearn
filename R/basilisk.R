5d9246a66c71246683ac8b3b3dedb77e23118262
Vince Carey <stvjc@channing.harvard.edu>
2020-05-13 11:03:31 -0400

    use 0.23.0 for scikit-learn

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,5 +2,5 @@
 # necessary for python module control
 bsklenv <- basilisk::BasiliskEnvironment(envname="bsklenv",
     pkgname="BiocSklearn",
-    packages=c("scikit-learn==0.22.2post1", "h5py==2.10.0", "pandas==1.0.3"))
+    packages=c("scikit-learn==0.23.0", "h5py==2.10.0", "pandas==1.0.3"))
 
