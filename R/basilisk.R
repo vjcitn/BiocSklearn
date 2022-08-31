47a58713072f96af6ba23b7d5f05fa1e585396d2
Vince Carey <stvjc@channing.harvard.edu>
2022-08-31 10:31:38 -0400

    skPWD works, rlang in Depends should come out

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,5 +2,5 @@
 # necessary for python module control
 bsklenv <- basilisk::BasiliskEnvironment(envname="bsklenv",
     pkgname="BiocSklearn",
-    packages=c("python==3.8.10", "scikit-learn==1.1.0", "h5py==3.7.0", "pandas==1.4.3", "joblib==1.1.0"))
+    packages=c("scikit-learn==1.0.2", "h5py==3.6.0", "pandas==1.3.5", "joblib==1.0.0"))
 
