3fca39434eef1456c3026414bc1e3640f67cb9cc
vjcitn <stvjc@channing.harvard.edu>
2022-12-26 07:06:25 -0500

    had to pin version of numpy to 1.21 so that typeDict error did not arise

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,6 +2,6 @@
 # necessary for python module control
 bsklenv <- basilisk::BasiliskEnvironment(envname="bsklenv",
     pkgname="BiocSklearn",
-    packages=c("scikit-learn==1.0.2", "h5py==3.6.0", "pandas==1.3.5", "joblib==1.0.0",
+    packages=c("numpy==1.21", "scikit-learn==1.1.0", "h5py==3.6.0", "pandas==1.3.5", "joblib==1.0.0",
       "scipy==1.4.1"))
 
